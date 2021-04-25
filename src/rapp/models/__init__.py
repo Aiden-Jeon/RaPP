dcd830f0cf29eff298bab414e68ba6cb923133d6
Aiden-Jeon <ells2124@gmail.com>
2021-04-25 16:50:46 +0900

    add aae and vae

--- src/rapp/models/__init__.py
+++ src/rapp/models/__init__.py
@@ -1,2 +1,4 @@
 from .autoencoder import AutoEncoder
+from .variational_autoencoder import VariationalAutoEncoder
+from .adversarial_autoencoder import AdversarialAutoEncoder
 from .rapp import RaPP
